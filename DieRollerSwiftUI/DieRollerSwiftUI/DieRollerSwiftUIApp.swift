e5d064bd911de4cc3c8469eb9cfc7ad03d40eb83
KyleSullivan1 <158103578+KyleSullivan1@users.noreply.github.com>
2024-03-21 21:18:38 -0400

    Add files via upload

---
+++ DieRollerSwiftUI/DieRollerSwiftUI/DieRollerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DieRollerSwiftUIApp.swift
+//  DieRollerSwiftUI
+//
+//  Created by user250839 on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DieRollerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
